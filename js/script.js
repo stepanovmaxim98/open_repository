1ac20149744cbd04a8f0d540dc04b3d5f040b03c
Maxim <stepanovmaxim98@yandex.ru>
2022-10-27 22:02:10 +0300

    Vlojenie cicle

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 "use strict";
-
-let num = 50;
-
-// while (num <= 55){
-//     console.log(num);
-//     num++;
-// }
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
-
-for (let i = 1; i<8; i++){
-    if (i === 6){
-        // break;
-        continue;
+// задача на вывод звездочек
+let result = '';
+const lenght = 7;
+for (let i = 1; i < lenght; i++){
+    for( let j = 0; j < i; j++){
+        result += "*";
     }
-    console.log(i);
-    num++;
+
+    result += '\n';
+}
+
+console.log(result);
+// прерывания цикла 
+first: for(let i=0; i<3; i++){
+    console.log(`First_level: ${i}`);
+    for (let j = 0; j < 3; j++){
+        console.log(`Second_level: ${j}`);
+        for (let k = 0; k < 5; k++){
+            if(k === 2) break first;
+            console.log(`Third_level: ${k}`);
+        }
+    }
 }
